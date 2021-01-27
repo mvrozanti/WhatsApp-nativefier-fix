56a8b8701dc317a36019263f071e63a86b6e4f9b
Marcelo <mvrozanti@hotmail.com>
2021-01-26 22:02:17 -0300

    add dark theme

--- fix.sh
+++ fix.sh
@@ -6,8 +6,8 @@
 
 path=`pwd`
 
-wget https://raw.githubusercontent.com/DocBox12/WhatsApp-nativefier-fix/master/whatsapp_fix.js
+# if [ ! -f whatsapp_fix.js ]; then
+#     wget https://raw.githubusercontent.com/DocBox12/WhatsApp-nativefier-fix/master/whatsapp_fix.js
+# fi
 
 nativefier --name "WhatsApp" --inject $path/whatsapp_fix.js "https://web.whatsapp.com"
-
-rm $path/whatsapp_fix.js
